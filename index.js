7454a93ab32536511dc4698d75a4349ae2a88261
Sam Arita <arita.sam@student.moringaschool.com>
2022-08-19 13:48:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+  function superbowlWin(collection){
+    for (const i of collection){
+        if (i.result === "W")
+        return i.year
+    }
+  }
+  superbowlWin(record)
